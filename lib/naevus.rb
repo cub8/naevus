63c1e97827a4d697b90c8d721e7da316c49f973f
cub8 <tsubait8@gmail.com>
2023-11-25 14:51:12 +0100

    Init commit

---
+++ lib/naevus.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "naevus/version"
+
+module Naevus
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/naevus_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Naevus do
+  it "has a version number" do
+    expect(Naevus::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
